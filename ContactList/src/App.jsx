70a6a7a56308bd48ce9ee375b637f3f7e9951dc7
Your Name <jasonbataillon@gmail.com>
2024-10-29 12:15:33 -0400

    Completed most everything up to passing props.

---
+++ ContactList/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import ContactList from './components/ContactList';
+import { useState } from 'react';
+
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+  return (
+    <>
+      {selectedContactId ? (
+        <div>Selected Contact View</div>
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
+    </>
+  );
+}
